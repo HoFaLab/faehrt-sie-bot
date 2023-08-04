e38be5bf550b25504fa98ee2c50d3c2d778505ea
Andre@HCU <andre.landwehr@hcu-hamburg.de>
2023-08-04 09:29:59 +0200

    wrap tide checking in try/except and report errors

--- main.py
+++ main.py
@@ -27,25 +27,30 @@ if __name__ == "__main__":
     tide_data = None
 
     while True:
-        if not tide_data or check_for_new_data(tide_data.forecast_creation_date):
-            tide_data = TideData()
-            if (
-                hasattr(tide_data, "disruption_period")
-                and tide_data.disruption_period.disruption_during_service_time()
-                and tide_data.disruption_period.get_disruption_length_minutes() > 30
-            ):
-                # check disruption period is already known.
+        # check tide
+        try:
+            if not tide_data or check_for_new_data(tide_data.forecast_creation_date):
+                tide_data = TideData()
                 if (
-                    not end_time_last_disruption
-                    or end_time_last_disruption < tide_data.disruption_period.end_time
+                    hasattr(tide_data, "disruption_period")
+                    and tide_data.disruption_period.disruption_during_service_time()
+                    and tide_data.disruption_period.get_disruption_length_minutes() > 30
                 ):
-                    post_message_to_telegram(tide_data.get_disruption_warn_msg())
+                    # check disruption period is already known.
+                    if (
+                        not end_time_last_disruption
+                        or end_time_last_disruption < tide_data.disruption_period.end_time
+                    ):
+                        post_message_to_telegram(tide_data.get_disruption_warn_msg())
 
-                if tide_data.is_time_to_remind() and not tide_data.reminder_sent():
-                    post_message_to_telegram(tide_data.get_reminder_msg())
-                    tide_data.reminder_sent = True
+                    if tide_data.is_time_to_remind() and not tide_data.reminder_sent():
+                        post_message_to_telegram(tide_data.get_reminder_msg())
+                        tide_data.reminder_sent = True
 
-                end_time_last_disruption = tide_data.disruption_period.end_time
+                    end_time_last_disruption = tide_data.disruption_period.end_time
+        except Exception as e:
+            print(e)
+            post_message_to_telegram(f"Error while checking tide: {e}", post_to_admin_group=True)
 
         # check tweets
         try:
